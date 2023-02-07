f28c7eaef218978eaba37ed7d314b70fa162337c
Kim <subum12345@gmail.com>
2023-02-07 21:50:21 +0900

    nextjs 연습

--- Nextjs_practice/nextjs-blog/src/pages/cart/[[...date]].js
+++ Nextjs_practice/nextjs-blog/src/pages/cart/[[...date]].js
@@ -1,5 +1,6 @@
 import Layout from 'components/Layout'
 import SubLayout from 'components/SubLayout'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '/styles/Home.module.css'
 
@@ -10,6 +11,9 @@ export default function CartDateSlug() {
   return (
     <>
       <h1 className={styles.title}>Cart Date Slug {JSON.stringify(date)}</h1>
+      <Link href="/cart/2023/02/07">2023년 2월 7일로</Link>
+      <br/>
+      <button onClick={()=>router.push('/cart/2023/02/01')}>2023년 2월 1일로</button>
     </>
   )
 }
