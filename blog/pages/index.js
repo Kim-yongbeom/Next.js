2d34fd9fe9222530790ebc299aed79d0a0f336cf
Kim <subum12345@gmail.com>
2023-02-22 23:00:04 +0900

    Next.js 연습

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1 className={styles.title}>
+          Read this post <Link href="/posts/first-post">첫번째 글</Link>
+        </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -48,6 +52,10 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+      <h1 className={styles.title}>
+        Read this post <Link href="/posts/second-post">두번째 글</Link>
+      </h1>
 
       <footer>
         <a
