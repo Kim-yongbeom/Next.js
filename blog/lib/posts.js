808fbdd00ccb70d3a312f36e5bdd7d76f284edf3
Kim <subum12345@gmail.com>
2023-03-09 20:57:15 +0900

    Nextjs 연습

--- blog/lib/posts.js
+++ blog/lib/posts.js
@@ -2,16 +2,21 @@ import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
 
+// root에 posts 경로를 뜻함 (posts 폴더)
 const postsDirectory = path.join(precess.cwd(), 'posts')
 
 export function getSortedPostsData() {
+    // file 이름을 가져오는 node 의 라이브러리(fs)
     const fileNames = fs.readdirSync(postsDirectory)
     const allPostsData = fileNames.map((fileNames)=>{
+        // .md 라는 텍스트를 지움
         const id = fileNames.replace(/\.md$/, '')
 
         const fullPath = path.join(postsDirectory, fileNames)
+        // 파일 읽어줌
         const fileContents = fs.readFileSync(fullPath, 'utf8')
 
+        // 메타 데이터 가져옴
         const matterResult = matter(fileContents)
 
         return {
