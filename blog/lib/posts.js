d360d93b5bb4fb0ba79f92087581bb0199db6f6a
Kim <subum12345@gmail.com>
2023-03-11 13:41:22 +0900

    nextjs 연습

--- blog/lib/posts.js
+++ blog/lib/posts.js
@@ -3,7 +3,7 @@ import path from "path"
 import matter from "gray-matter"
 
 // root에 posts 경로를 뜻함 (posts 폴더)
-const postsDirectory = path.join(precess.cwd(), 'posts')
+const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
     // file 이름을 가져오는 node 의 라이브러리(fs)
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,8 +1,44 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Layout, { siteTitle } from '../components/layout';
+// import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
+// ssg
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData,
+//     }
+//   }
+// }
+
+// ssr
+// export async function getServerSideProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData,
+//     }
+//   }
+// }
+
+// csr 일때는 {allPostsData} props 를 빼고
+// ssg, ssr 일때는 {allPostsData} props 를 넣는다
 export default function Home() {
+
+  // csr
+  const [allPostsData, setAllPostsData] = useState([])
+  useEffect(()=>{
+    fetch('/api/posts')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(JSON.stringify(data))
+      setAllPostsData(data.allPostsData) 
+    })
+  },[])
+
   return (
     <Layout home>
       <Head>
@@ -14,6 +50,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
