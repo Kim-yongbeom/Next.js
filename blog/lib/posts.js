cfee2d87d8c8f19c9ea593fcae1d132eb61de961
Kim <subum12345@gmail.com>
2023-03-14 20:38:35 +0900

    page post [slug] 바꿔주기

--- blog/lib/posts.js
+++ blog/lib/posts.js
@@ -34,3 +34,14 @@ export function getSortedPostsData() {
         }
     })
 }
+
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory);
+    return fileNames.map((fileNames) => {
+        return {
+            params: {
+                id: fileNames.replace(/\.md$/, ''),
+            }
+        }
+    })
+}
