3ce5738ef979400e3f03fdbfb1455ab818a10bef
Kim <subum12345@gmail.com>
2023-03-15 23:51:24 +0900

    nextjs 연습

---
+++ blog/pages/posts/[id].js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({params}) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    }
+  }
+}
+
+export default function Post({postData}) {
+  return (
+    <Layout>
+      {postData.title}
+      <br/>
+      {postData.id}
+      <br/>
+      {postData.date}
+    </Layout>
+  )
+}
