423ba322641205955031c893b26e5c2a57101e84
Kim <subum12345@gmail.com>
2023-04-13 23:30:47 +0900

    nextjs

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,11 +1,17 @@
 import Layout from '@/components/Layout'
 import '../styles/globals.css'
 import { useRouter } from 'next/router'
+import {format, formatDistanceToNow} from 'date-fns'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
+  const [visitedTime] = useState(new Date())
   return (
     <Layout home={router.pathname === '/'}>
+      <div>
+        visited: {formatDistanceToNow(new Date(visitedTime), {addSuffix: true, includeSeconds: true})}
+      </div>
       <Component {...pageProps} pathname={router.pathname} />
     </Layout>
   )
--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -40,7 +40,7 @@ export async function getStaticProps({ params, preview }) {
 
 const components = { Button, CodeBlock }
 
-export default function Post({ postData }) {
+export default function Post({ postData, pathname }) {
   const router = useRouter()
 
   if (router.isFallback) {
@@ -49,6 +49,7 @@ export default function Post({ postData }) {
 
   return (
     <article>
+      <h2>pathname: {pathname}</h2>
       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
       <div className={utilStyles.lightText}>
         <Date dateString={postData.date} />
