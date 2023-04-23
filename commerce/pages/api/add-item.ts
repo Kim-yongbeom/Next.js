95dff7e2a2ea5610a11fa1dddcab0e5b170866fb
Kim <subum12345@gmail.com>
2023-04-23 18:29:15 +0900

    nextjs

--- commerce/pages/api/add-item.ts
+++ commerce/pages/api/add-item.ts
@@ -2,23 +2,49 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import {Client} from '@notionhq/client'
 
 const notion = new Client({
-  auth: 'secret_oLv71qbMHnSskWbgs4uFHdSwQhGKC6HeGU1o8OxSkUh'
+  auth: 'secret_tKgpJgfbzv3p66i35xuIicp9n6SrcpMQpramxi1XRyc'
 })
 
 const databaseId = '63dce21733464284a93b92b8f61fd35c'
 
 async function addItem(name: string) {
-  
+  try {
+    const response = await notion.pages.create({
+      parent: {database_id: databaseId},
+      properties: {
+        title: [
+          {
+            text: {
+              content: name,
+            }
+          }
+        ]
+      }
+    })
+    console.log(response)
+  } catch (error) {
+    console.error(JSON.stringify(error))
+  }
 }
 
 type Data = {
   message: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const {name} = req.query
-  res.status(200).json({ message: `Success ${name} added` })
+
+  if(name == null) {
+    return res.status(400).json({message: 'No name'})
+  }
+
+  try {
+    await addItem(String(name))
+    res.status(200).json({ message: `Success ${name} added` })
+  } catch (error) {
+    res.status(400).json({ message: `Failed ${name} added` })
+  }
 }
