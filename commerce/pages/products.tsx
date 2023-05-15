8102d080a7d8c72ebde2c17424363a3eab19dd0c
Kim <subum12345@gmail.com>
2023-05-15 21:36:58 +0900

    image 다루기

---
+++ commerce/pages/products.tsx
@@ -0,0 +1,42 @@
+// import ImageGallery from 'react-image-gallery';
+import Carousel from "nuka-carousel";
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1016/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1016/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1013/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1013/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+export default function products() {
+    // return <ImageGallery items={images} />;
+    return(
+        <Carousel>
+            {images.map((item) => <img key={item.original} src={item.original} alt="image" />)}
+        </Carousel>
+    )
+}
