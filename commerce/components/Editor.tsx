988ec1c94e598b25039e6ae27c52a7bca6ef2601
Kim <subum12345@gmail.com>
2023-05-27 22:45:40 +0900

    nextjs

--- commerce/components/Editor.tsx
+++ commerce/components/Editor.tsx
@@ -1,5 +1,9 @@
+import styled from "@emotion/styled"
 import dynamic from "next/dynamic"
-import { EditorProps } from "react-draft-wysiwyg"
+import { Dispatch, SetStateAction } from "react"
+import { EditorProps, EditorState } from "react-draft-wysiwyg"
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import Button from "./Button"
 
 const Editor = dynamic<EditorProps>(
     () => import('react-draft-wysiwyg').then((module) => module.Editor),
@@ -8,8 +12,25 @@ const Editor = dynamic<EditorProps>(
     }
 )
 
-export default function CustomEditor() {
+export default function CustomEditor(
+      {editorState, readOnly = false, onEditorStateChange, onSave}: 
+      {editorState: EditorState, readOnly?: boolean, onEditorStateChange ?: Dispatch<SetStateAction<EditorState | undefined>>, onSave : () => void}
+    ) 
+  {
   return (
-    <Editor/>
+    <Wrapper>
+    <Editor
+      editorState={editorState}
+      toolbarClassName="toolbarClassName"
+      wrapperClassName="wrapperClassName"
+      editorClassName="editorClassName"
+      onEditorStateChange={onEditorStateChange}
+    />
+    {!readOnly && <Button onClick={onSave}>Save</Button>}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  padding: 16px;
+`;
