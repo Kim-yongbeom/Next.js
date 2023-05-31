21b0741cf826ed9486bbd6514d2d8984b3693369
Kim <subum12345@gmail.com>
2023-05-31 23:57:50 +0900

    prisma 수정중

---
+++ commerce/pages/api/update-product.ts
@@ -0,0 +1,42 @@
+// prisma는 지운 상태
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function updateProduct(id: number, contents: string) {
+  try {
+    const response = await prisma.products.update({
+      where: {
+        id: id,
+      },
+      data: {
+        contents: contents,
+      }
+    })
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type Data = {
+  items?: any
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {id, contents} = req.body
+  if(id == null || contents == null) {
+    res.status(400).json({message: 'no id or contents'})
+  }
+  try {
+    const products = await updateProduct(Number(id), contents)
+    res.status(200).json({ items: products, message: `Success` })
+  } catch (error) {
+    res.status(400).json({ message: `Failed` })
+  }
+}
